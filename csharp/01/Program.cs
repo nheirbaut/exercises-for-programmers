71cb2a6125c708314d4cd030a279ea2d3a12befc
Niels Heirbaut <niels@opilo.nl>
2017-02-05 13:26:15 +0100

    Initial commit of exercise 01 in C#

---
+++ csharp/01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}, nice to meet you!");
+        }
+    }
+}
