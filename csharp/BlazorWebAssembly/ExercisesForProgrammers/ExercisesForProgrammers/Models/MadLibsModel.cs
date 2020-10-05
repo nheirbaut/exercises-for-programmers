77d53d4d27ee5f20cbcda6919f3f4d2e6bcc03b2
Niels Heirbaut <niels@opilo.nl>
2020-10-05 21:26:14 +0200

    Add exercise 04

---
+++ csharp/BlazorWebAssembly/ExercisesForProgrammers/ExercisesForProgrammers/Models/MadLibsModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ExercisesForProgrammers.Models
+{
+    public class MadLibsModel
+    {
+        public string Noun { get; set; }
+        public string Verb { get; set; }
+        public string Adjective { get; set; }
+        public string Adverb { get; set; }
+
+        public string Story => $"Do you {Verb} your {Adjective} {Noun} {Adverb}? That's hilarious!";
+    }
+}
